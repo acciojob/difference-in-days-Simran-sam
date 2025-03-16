1cf5c78036eef3fd7b1769348c03a25f6be71031
Simran Pathan <97839297+Simran-sam@users.noreply.github.com>
2025-03-16 12:46:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
-	let diffInTime= date2.getTime() - date1.getTime();
-	let days=Math.round(diffInTime/(1000*60*60*24);
+  //   write your code 
+	const d1 = new Date(date1);
+	const d2= new Date(date2);
+	let diffInTime= d2 - d1;
+	let days=diffInTime/(1000*60*60*24);
 						return days;
-)};
+)
+console.log(dateDiffInDays("2022-03-01", "2022-03-15")); // 14
+        console.log(dateDiffInDays("2022-03-01", "2022-03-01")); // 0
+        console.log(dateDiffInDays("2022-03-15", "2022-03-01")); // -14
+
+};
 
 // Do not change the code below.
 const dateOne = prompt("Enter Start Date.");
